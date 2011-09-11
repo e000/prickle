32a8c6f9214296ab26d1be46158d511dcfba04c0
e <e@tr0ll.in>
2011-09-11 01:28:38 +0000

    added __init__.py

---
+++ __init__.py
@@ -0,0 +1,12 @@
+"""
+    stats
+    ~~~~~
+    a simple python rddtool based stats graph app,
+    or the easy alternative option to cacti
+    
+    :copyright: (c) 2001 Edgeworth E. Euler
+    :license: BSD!
+    
+"""
+
+from .stats import Stats
