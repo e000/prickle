52d050997aadd12a4831cf1f8a14659b4a66d24a
e <e@tr0ll.in>
2011-09-29 06:32:55 +0000

    txmysql is worthless

--- txmysql/error.py
+++
@@ -1,4 +0,0 @@
-class MySQLError(Exception):
-    def __init__(self, message, errno=None, sqlstate=None, query=None):
-        super(MySQLError, self).__init__(message, errno, sqlstate, query)
-        self.msg, self.errno, self.sqlstate, self.query = message, errno, sqlstate, query
